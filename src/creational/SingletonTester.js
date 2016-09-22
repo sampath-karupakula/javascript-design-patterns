4e798fe43d81a059a77a8e3af08dbd4d95af409d
sampath-karupakula <karupakulasampath@gmail.com>
2016-09-22 16:31:19 +0530

    added comments.

--- src/creational/SingletonTester.js
+++ src/creational/SingletonTester.js
@@ -1,18 +1,18 @@
-var SingletonTester = (function() {
-    function Singleton(options) {
+var SingletonTester = (function() { // A simple tester function for Singleton.
+    function Singleton(options) { // internal constructor function
         options = options || {};
         this.name = "SingletonTester";
         this.pointX = options.pointX || 4;
         this.pointY = options.pointY || 4;
     }
 
-    var instance;
+    var instance; // instance of instance object.
 
 
-    var _static = {
-        name:"SingletonTester",
-        getInstance : function(options){
-            if(instance === undefined){
+    var _static = { // static object, which return on using class or function name.
+        name: "SingletonTester",
+        getInstance: function(options) {
+            if (instance === undefined) {
                 instance = new Singleton(options);
             }
             return instance;
--- src/creational/singleton.js
+++ src/creational/singleton.js
@@ -2,23 +2,25 @@ var singleton = (function() {
 
     var initiated;
 
-    function init() {
-        return {
+    function init() { // init method to create a singleton object. or internal constructor.
+        return { // an object which exposed internal state to external world.
             publicFunction: privateFunction,
             publicField: "public field"
         };
     }
 
+    //private content. which is not visisble outside.
     var privateField = "private field";
 
     function privateFunction() {
         return "called private function";
     }
 
+    // a closure, which returns internal state by creating upon calling on getInstance method.
     return {
         getInstance: function() {
-            if (!initiated) {//lazy initialization.
-                initiated = init();
+            if (!initiated) { //lazy initialization. if initialized return previously created object, which hold singleton nature.
+                initiated = init();//otherwise initialize it.
             }
             return initiated;
         }
